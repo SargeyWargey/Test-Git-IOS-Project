8d7e62c6f029a6df72213b882c19d448d6cd6f2d
Joshua Sargent <Sargent@Joshuas-MacBook-Pro.local>
2014-05-12 18:55:01 -0600

    added a test

---
+++ JASTest.h
@@ -0,0 +1,13 @@
+//
+//  JASTest.h
+//  Git Test
+//
+//  Created by Joshua Sargent on 5/12/14.
+//  Copyright (c) 2014 DreamLiveCapture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JASTest : NSObject
+
+@end
---
+++ JASTest.m
@@ -0,0 +1,13 @@
+//
+//  JASTest.m
+//  Git Test
+//
+//  Created by Joshua Sargent on 5/12/14.
+//  Copyright (c) 2014 DreamLiveCapture. All rights reserved.
+//
+
+#import "JASTest.h"
+
+@implementation JASTest
+
+@end
